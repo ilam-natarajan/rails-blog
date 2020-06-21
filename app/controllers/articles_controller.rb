1c17d28104dceb1623708b01be202396afffdd03
Ilamparithi <ilamparithi.natarajan@wipro.com>
2020-06-21 22:37:36 +0100

    add comments functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+	http_basic_authenticate_with name: "ilam", password: "secret", except: [:index, :show]
+	
 	def index
     @articles = Article.all
   end
