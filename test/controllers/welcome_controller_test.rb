3c55c496909275d1c40ddbc9df333747cb91561b
Ilamparithi Natarajan <28389350+ilamparithiNatarajan@users.noreply.github.com>
2020-07-25 13:54:51 +0100

    Update welcome_controller_test.rb

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -4,6 +4,7 @@ class WelcomeControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get welcome_index_url
     assert_response :success
+    puts "test success. well done"
   end
 
 end
